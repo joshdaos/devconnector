0357671f3201c21d5a006078fc30b177868daca6
JoshDaos <joshuadaos1998@gmail.com>
2021-10-04 23:04:49 -0700

    security changes

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const app = express();
 // connecting database
 connectDB();
 
-app.get('/', (req, res) => res.send('API RUNNING'))
+app.get('/', (req, res) => res.send('API RUNNING'));
+
+// defining routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
 
 const PORT = process.env.PORT || 5000
 
